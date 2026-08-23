f2d20c928ae9e594818925528b53a62641ebbdcf
MITI-Apps <okekem186@gmail.com>
2026-08-24 00:22:59 +0100

    changed secure

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT) || 587,
-  secure: false,
+  secure: true,
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
