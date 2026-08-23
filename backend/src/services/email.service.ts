57f1ea47f21b9df3006e96311c34699d7331070d
MITI-Apps <okekem186@gmail.com>
2026-08-24 00:50:33 +0100

    email host back to original state

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -2,14 +2,13 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT) || 465,
-  secure: true,
-  family: 4,
+  port: Number(process.env.EMAIL_PORT) || 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-} as any );
+});
 
 const verifyTransporter = async (): Promise<void> => {
   try {
